19952756852b57bef8b656cfb08a0649fafad5c3
Oleg Polyanskiy <ol.polyanskiy@gmail.com>
2021-12-11 21:28:01 +0300

    feat: add mobx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,7 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import React from 'react';
+import type { AppProps } from 'next/app';
+import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />;
 
-export default MyApp
+export default MyApp;
