29ea2111f2b4163dc2c78e635f8b985344784361
Oleg Polyanskiy <ol.polyanskiy@gmail.com>
2022-02-13 00:55:21 +0300

    perf: add custom select in stats

--- components/stats/TopBar/TopBar.tsx
+++ components/stats/TopBar/TopBar.tsx
@@ -1,21 +1,39 @@
 import React from 'react';
 import classNames from 'classnames';
+import Select from 'react-select';
 import styles from './topbar.module.css';
 
 interface Props {
   extraClass?: string,
 }
 
+const options = [
+  { value: 'current', label: 'Эта неделя' },
+  { value: 'last', label: 'Прошедшая неделя' },
+  { value: 'before-last', label: '2 недели назад' },
+];
+
+const customStyles = {
+  indicatorSeparator: (provided: {}) => ({
+    ...provided,
+    display: 'none',
+  }),
+  container: (provided: {}) => ({
+    ...provided,
+    width: 300,
+  }),
+};
+
 export const TopBar = ({ extraClass }: Props) => {
   const containerClass = classNames(extraClass, styles.container);
   return (
     <section className={containerClass}>
       <h2 className={styles.title}>Ваша активность</h2>
-      <select title="выбор периода">
-        <option value="">Эта неделя</option>
-        <option value="">Прошедшая неделя</option>
-        <option value="">2 недели назад</option>
-      </select>
+      <Select
+        defaultValue={options[0]}
+        styles={customStyles}
+        options={options}
+      />
     </section>
   );
 };
